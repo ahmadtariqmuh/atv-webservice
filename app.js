b6374fac8ff9130ec465f440f00939faee182a5d
ahmadtariqmuh <ahmadtariqmuh@gmail.com>
2024-10-09 15:08:48 +0300

    commit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+});
 
 app.use(express.json());
 
